8e8e0ff3e83acd0aa6b74de91ab95becc84ad2c6
meesooqa <meesooqa@hotmail.com>
2026-09-17 16:33:25 +0300

    add script for generating MP3 files with cover image and metadata

---
+++ internal/media/testdata/mp3gen.sh
@@ -0,0 +1,15 @@
+ffmpeg \
+  -f lavfi \
+  -i anullsrc=r=44100:cl=mono \
+  -i cover.png \
+  -t 3 \
+  -map 0:a -map 1:v \
+  -metadata title="Test Audio Track" \
+  -metadata artist="Test Artist" \
+  -metadata album_artist="Test Album Artist" \
+  -metadata comment="Test Comment" \
+  -codec:a libmp3lame -b:a 32k \
+  -codec:v mjpeg \
+  -b:a 32k \
+  -id3v2_version 3 \
+  three_sec_tagged_with_cover.mp3
