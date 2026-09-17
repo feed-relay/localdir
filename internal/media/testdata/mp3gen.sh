849d4eb40a5bfa752181b8ac6aaf67b11c7fbc04
meesooqa <meesooqa@hotmail.com>
2026-09-17 17:15:14 +0300

    remove redundant audio bitrate setting in mp3gen script

--- internal/media/testdata/mp3gen.sh
+++ internal/media/testdata/mp3gen.sh
@@ -11,7 +11,7 @@ ffmpeg \
   -metadata artist="Test Artist" \
   -metadata album_artist="Test Album Artist" \
   -metadata comment="Test Comment" \
-  -codec:a libmp3lame -b:a 32k \
+  -codec:a libmp3lame \
   -codec:v mjpeg \
   -b:a 32k \
   -id3v2_version 3 \
