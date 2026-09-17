796a3139aaad9c2a97644bd96483c64150ae22e4
meesooqa <meesooqa@hotmail.com>
2026-09-17 20:02:21 +0300

    refactor finder initialization: update duration reader to use NewFfprobeDurationReader

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -6,8 +6,6 @@ import (
 	"log"
 	"log/slog"
 
-	"github.com/feed-relay/localdir/internal/media"
-	"github.com/feed-relay/localdir/internal/writer"
 	"github.com/meesooqa/go-cfg"
 	"github.com/meesooqa/go-lgr"
 
@@ -15,6 +13,8 @@ import (
 
 	"github.com/feed-relay/localdir/internal/config"
 	"github.com/feed-relay/localdir/internal/fs"
+	"github.com/feed-relay/localdir/internal/media"
+	"github.com/feed-relay/localdir/internal/writer"
 	"github.com/feed-relay/localdir/pkg/provider"
 )
 
@@ -34,7 +34,7 @@ func main() {
 		feeds[i] = &feed
 	}
 
-	finder := fs.NewFinder(media.NewMetadataReader(), media.NewDurationReader())
+	finder := fs.NewFinder(media.NewMetadataReader(), media.NewFfprobeDurationReader(&media.ExecCommandRunner{}))
 	p := provider.NewProvider(conf, finder)
 	rssFeeds, err := p.Feeds(context.Background(), feeds)
 	if err != nil {
