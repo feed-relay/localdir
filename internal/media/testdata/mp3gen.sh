f9794684e46a1624a786c9f0567bc173d07dd656
meesooqa <meesooqa@hotmail.com>
2026-09-17 19:17:30 +0300

    remove test comment metadata from MP3 generation script

--- internal/media/testdata/mp3gen.sh
+++ internal/media/testdata/mp3gen.sh
@@ -10,7 +10,6 @@ ffmpeg \
   -metadata title="Test Audio Track" \
   -metadata artist="Test Artist" \
   -metadata album_artist="Test Album Artist" \
-  -metadata comment="Test Comment" \
   -codec:a libmp3lame \
   -codec:v mjpeg \
   -b:a 32k \
